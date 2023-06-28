9f0e0c21be43ba4fee91abb6d73eb40adeb03809
kahmingt <kahmingt@gmail.com>
2023-06-28 19:50:59 +0800

    Enabled Order CRUD functionality

---
+++ Models/Alert.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp.Models
+{
+    public enum AlertType { Danger, Info, Success, Warning }
+
+    public class Alerts
+    {
+        public string Type { get; set; }
+        public string Message { get; set; }
+    }
+}
